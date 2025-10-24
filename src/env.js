2a8e4fc0a4c153100173c5e472c7e5c1d71cccbc
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-10-24 13:36:08 -0300

    feat: ✨ adicionar variáveis de ambiente para `DATABASE`

--- src/env.js
+++ src/env.js
@@ -3,6 +3,8 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
+    DATABASE_URL: z.string().url(),
+    DATABASE_SCHEMA: z.string().default(""),
     AUTH_SECRET:
       process.env.NODE_ENV === "production"
         ? z.string()
