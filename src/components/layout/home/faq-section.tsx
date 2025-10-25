1f5387ea47285b079f9996b84a60a2f50e4dddfc
JoaoDias06 <joaozinhopd06@gmail.com>
2025-10-24 21:00:04 -0300

    refactor: ♻ implementar conteudo da sessão de FAQ

--- src/components/layout/home/faq-section.tsx
+++ src/components/layout/home/faq-section.tsx
@@ -1,5 +1,26 @@
+import { DecorativeText } from "~/components/ui/decorative-text";
 import { Section } from "~/components/ui/section";
+import { faqMock } from "~/features/faq/mock";
 
 export function FAQSection() {
-  return <Section id="faq">FAQ</Section>;
+  return (
+    <Section id="faq">
+      <div className="flex flex-col items-center gap-2">
+        <h2 className="text-center">
+          <DecorativeText leading="~$ {" trailing="} //">
+            Perguntas Frequentes
+          </DecorativeText>
+        </h2>
+      </div>
+
+      <div className="max-w-2xl space-y-4">
+        {faqMock.map(({ question, answer }, index) => (
+          <div key={index}>
+            <h3 className="font-semibold">{question}</h3>
+            <p className="text-muted text-base">{answer}</p>
+          </div>
+        ))}
+      </div>
+    </Section>
+  );
 }
---
+++ src/features/faq/mock.ts
@@ -0,0 +1,28 @@
+// TODO: buscar dinâmicamente
+export const faqMock = [
+  {
+    question: "Como faço para me inscrever no curso?",
+    answer:
+      "Você pode acompanhar as aulas publicadas gratuitamente por aqui no site. As aulas presenciais são oferecidas em parceria com escolas parceiras. Se você é uma escola interessada, entre em contato conosco para mais informações.",
+  },
+  {
+    question: "Preciso saber programar para fazer o curso?",
+    answer:
+      "Não, o curso é projetado para iniciantes sem experiência prévia em programação.",
+  },
+  {
+    question: "Pra quem é este curso?",
+    answer:
+      "Ele foi desenvolvido pensando em alunos do Ensino Médio, mas é ideal para qualquer pessoa interessada em aprender programação, independentemente da idade ou formação.",
+  },
+  {
+    question: "O que vou construir no final?",
+    answer:
+      "Ao final do curso, você terá desenvolvido um projeto completo que poderá adicionar ao seu portfólio, demonstrando suas novas habilidades em programação.",
+  },
+  {
+    question: "Como funciona a metodologia?",
+    answer:
+      "O curso combina aulas teóricas com projetos práticos, permitindo que você aprenda conceitos fundamentais enquanto aplica o conhecimento em situações reais, além de dinâmicas fora do ambiente virtual para reforçar o aprendizado.",
+  },
+];
