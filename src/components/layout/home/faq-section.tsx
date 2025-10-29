52fb962a6d611cb1cb0955bba5cdfa78a0cb44bb
gilberto-alves-leme <gilbertoalvesnetto@gmail.com>
2025-10-29 17:07:41 -0300

    refactor: ♻️ substituir dados mock por dados dinâmicos da API para seção de perguntas frequentes

--- src/components/layout/home/faq-section.tsx
+++ src/components/layout/home/faq-section.tsx
@@ -1,8 +1,10 @@
 import { DecorativeText } from "~/components/ui/decorative-text";
 import { Section } from "~/components/ui/section";
-import { faqMock } from "~/features/faq/mock";
+import { api } from "~/trpc/server";
 
-export function FAQSection() {
+export async function FAQSection() {
+  const faq = await api.faq.list();
+
   return (
     <Section id="faq">
       <div className="flex flex-col items-center gap-2">
@@ -14,7 +16,13 @@ export function FAQSection() {
       </div>
 
       <div className="max-w-2xl space-y-4">
-        {faqMock.map(({ question, answer }, index) => (
+        {faq.length === 0 && (
+          <p className="text-muted text-center">
+            Nenhuma pergunta frequente cadastrada.
+          </p>
+        )}
+
+        {faq.map(({ question, answer }, index) => (
           <div key={index}>
             <h3 className="font-semibold">{question}</h3>
             <p className="text-muted text-base">{answer}</p>
