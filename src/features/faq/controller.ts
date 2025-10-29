f78819079e809f177626e2c98a01afee307ff7f7
Ferrazz843 <gustavoferrazz843@gmail.com>
2025-10-29 14:27:21 -0300

    feat: ✨ implementar consulta para perguntas frequentes

--- src/features/faq/controller.ts
+++ src/features/faq/controller.ts
@@ -1,3 +1,16 @@
-import { createTRPCRouter } from "~/server/api/trpc";
+import { asc } from "drizzle-orm";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const faqRouter = createTRPCRouter({});
+export const faqRouter = createTRPCRouter({
+  list: publicProcedure.query(async ({ ctx }) => {
+    const faqs = await ctx.db.query.frequentlyAskedQuestions.findMany({
+      orderBy: (faq) => [asc(faq.order), asc(faq.updatedAt)],
+    });
+
+    // Omite campos desnecessários para o cliente
+    return faqs.map(({ question, answer }) => ({
+      question,
+      answer,
+    }));
+  }),
+});
