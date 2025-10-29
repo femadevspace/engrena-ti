8ae01dc52cd07ae3272551fb5bcc977759fefd37
GabrielCamargoGC <gabrielcamargocamilo18@gmail.com>
2025-10-29 14:34:44 -0300

    fix: 🩹 adicionar relacionamento para `adminUser` no modelo de FAQ

--- src/features/faq/models.ts
+++ src/features/faq/models.ts
@@ -1,11 +1,34 @@
+import { relations } from "drizzle-orm";
 import { appSchema, baseSchema } from "~/server/db/base";
+import { adminUsers } from "../admin/models";
 
 export const frequentlyAskedQuestions = appSchema.table("faq", (d) => ({
   ...baseSchema,
   order: d.integer("order").notNull().default(0),
   question: d.text("question").notNull(),
   answer: d.text("answer").notNull(),
+
+  createdByAdminId: d
+    .uuid()
+    .references(() => adminUsers.id, { onDelete: "set null" }),
+  updatedByAdminId: d
+    .uuid()
+    .references(() => adminUsers.id, { onDelete: "set null" }),
 }));
 
 export type FrequentlyAskedQuestion =
   typeof frequentlyAskedQuestions.$inferSelect;
+
+export const frequentlyAskedQuestionsRelations = relations(
+  frequentlyAskedQuestions,
+  ({ one }) => ({
+    createdByAdmin: one(adminUsers, {
+      fields: [frequentlyAskedQuestions.createdByAdminId],
+      references: [adminUsers.id],
+    }),
+    updatedByAdmin: one(adminUsers, {
+      fields: [frequentlyAskedQuestions.updatedByAdminId],
+      references: [adminUsers.id],
+    }),
+  }),
+);
