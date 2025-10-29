52d25aef033cc2a5946f2f01e9091fc307016414
Ferrazz843 <gustavoferrazz843@gmail.com>
2025-10-29 14:26:58 -0300

    feat: ✨ implementar roteador para perguntas frequentes

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,9 @@
+import { faqRouter } from "~/features/faq/controller";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  faq: faqRouter,
+});
 
 export type AppRouter = typeof appRouter;
 
