dbe2178d7574a50acc525b449a1b6cb53d5a1919
FelipeMatth <fefe.mso@hotmail.com>
2025-10-29 14:39:09 -0300

    feat: ✨ adicionar funcionalidade de criação de perguntas frequentes

--- src/features/faq/controller.ts
+++ src/features/faq/controller.ts
@@ -1,7 +1,33 @@
 import { asc } from "drizzle-orm";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import z from "zod";
+import {
+  adminProcedure,
+  createTRPCRouter,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { frequentlyAskedQuestions } from "./models";
 
 export const faqRouter = createTRPCRouter({
+  create: adminProcedure
+    .input(
+      z.object({
+        question: z.string().nonempty(),
+        answer: z.string().nonempty(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const adminId = ctx.session.user.id;
+
+      await ctx.db.insert(frequentlyAskedQuestions).values({
+        question: input.question,
+        answer: input.answer,
+        createdByAdminId: adminId,
+        updatedByAdminId: adminId,
+      });
+
+      return { success: true, message: "FAQ registrada com sucesso!" };
+    }),
+
   list: publicProcedure.query(async ({ ctx }) => {
     const faqs = await ctx.db.query.frequentlyAskedQuestions.findMany({
       orderBy: (faq) => [asc(faq.order), asc(faq.updatedAt)],
