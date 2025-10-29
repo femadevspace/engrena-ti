54833c101dc40d6148ad6f2f35070ba0467d7653
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-10-29 16:32:40 -0300

    fix: 🩹 ajustar configuração de conexão com o banco de dados para desabilitar prefetch

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -9,7 +9,12 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+const conn =
+  globalForDb.conn ??
+  postgres(env.DATABASE_URL, {
+    // Desabilita prefetch pois não é suportado para o modo de pool "Transaction"
+    prepare: false,
+  });
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
