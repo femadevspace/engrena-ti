15f1ade0bac435b36721ea7622519dd6e406f832
gilberto-alves-leme <gilbertoalvesnetto@gmail.com>
2025-10-30 10:44:58 -0300

    refactor: 💄 refatorar componente `Date` para formatar a data/hora

--- src/components/ui/date.tsx
+++ src/components/ui/date.tsx
@@ -1,13 +1,33 @@
-function Date({ date }: { date: Date }) {
-  return (
-    <time dateTime={date.toISOString()}>
-      {date.toLocaleDateString("pt-BR", {
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric",
-      })}
-    </time>
-  );
+function formatDate(date: Date): string {
+  const now = new Date();
+
+  const seconds = Math.round((now.getTime() - date.getTime()) / 1000);
+  if (seconds < 60) return "Agora mesmo";
+
+  const minutes = Math.floor(seconds / 60);
+  if (minutes < 60) return `Há ${minutes} minuto${minutes > 1 ? "s" : ""}`;
+
+  const hours = Math.floor(minutes / 60);
+  if (hours < 24) return `Há ${hours} hora${hours > 1 ? "s" : ""}`;
+
+  const days = Math.floor(hours / 24);
+  if (days === 1)
+    return `Ontem às ${date.toLocaleTimeString("pt-BR", {
+      hour: "2-digit",
+      minute: "2-digit",
+    })}`;
+
+  return date.toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 }
 
-export { Date };
+function DateComponent({ date }: { date: Date }) {
+  return <time dateTime={date.toISOString()}>{formatDate(date)}</time>;
+}
+
+export { DateComponent as Date };
